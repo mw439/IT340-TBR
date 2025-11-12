83f22cb605e02d211b46cb0ceb075a6ada9093df
MengChengWang <mw439@njit.edu>
2025-11-12 00:28:37 -0500

    node modules testing w/front and backend

--- tbr-frontend/src/app/app.routes.ts
+++ tbr-frontend/src/app/app.routes.ts
@@ -1,34 +1,34 @@
-// import { Routes } from '@angular/router';
-
-// export const routes: Routes = [];
-
 import { Routes } from '@angular/router';
-import { LoginComponent } from './pages/login/login.component';
-import { RegisterComponent } from './pages/register/register.component';
-import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
-import { BrowseComponent } from './pages/browse/browse.component';
-import { SearchComponent } from './pages/search/search.component';
-import { CartComponent } from './pages/cart/cart.component';
-import { CheckoutComponent } from './pages/checkout/checkout.component';
-import { BookDetailsComponent } from './pages/book-details/book-details.component';
-import { ProfileComponent } from './pages/profile/profile.component';
-import { AboutComponent } from './pages/about/about.component';
-import { ContactComponent } from './pages/contact/contact.component';
-import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-export const routes: Routes = [
-  { path: '', redirectTo: '/browse', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'forgot-password', component: ForgotPasswordComponent },
-  { path: 'browse', component: BrowseComponent },
-  { path: 'search', component: SearchComponent },
-  { path: 'cart', component: CartComponent },
-  { path: 'checkout', component: CheckoutComponent },
-  { path: 'book-details', component: BookDetailsComponent },
-  { path: 'profile', component: ProfileComponent },
-  { path: 'about', component: AboutComponent },
-  { path: 'contact', component: ContactComponent },
-  { path: '**', component: NotFoundComponent }
-];
+export const routes: Routes = [];
+
+// import { Routes } from '@angular/router';
+// import { LoginComponent } from './pages/login/login.component';
+// import { RegisterComponent } from './pages/register/register.component';
+// import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
+// import { BrowseComponent } from './pages/browse/browse.component';
+// import { SearchComponent } from './pages/search/search.component';
+// import { CartComponent } from './pages/cart/cart.component';
+// import { CheckoutComponent } from './pages/checkout/checkout.component';
+// import { BookDetailsComponent } from './pages/book-details/book-details.component';
+// import { ProfileComponent } from './pages/profile/profile.component';
+// import { AboutComponent } from './pages/about/about.component';
+// import { ContactComponent } from './pages/contact/contact.component';
+// import { NotFoundComponent } from './pages/not-found/not-found.component';
+
+// export const routes: Routes = [
+//   { path: '', redirectTo: '/browse', pathMatch: 'full' },
+//   { path: 'login', component: LoginComponent },
+//   { path: 'register', component: RegisterComponent },
+//   { path: 'forgot-password', component: ForgotPasswordComponent },
+//   { path: 'browse', component: BrowseComponent },
+//   { path: 'search', component: SearchComponent },
+//   { path: 'cart', component: CartComponent },
+//   { path: 'checkout', component: CheckoutComponent },
+//   { path: 'book-details', component: BookDetailsComponent },
+//   { path: 'profile', component: ProfileComponent },
+//   { path: 'about', component: AboutComponent },
+//   { path: 'contact', component: ContactComponent },
+//   { path: '**', component: NotFoundComponent }
+// ];
 

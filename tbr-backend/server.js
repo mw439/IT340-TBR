d5b67bdeaf7de86ba860a34aaf3531a6a51c9d07
Mohsin Imtiaz <mi329@njit.edu>
2025-11-16 17:41:13 -0500

    Corrections made.

--- tbr-backend/server.js
+++ tbr-backend/server.js
@@ -1,25 +1,26 @@
-
-
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const cors = require"('cors');
+const cors = require('cors');
 const authRoutes = require('./routes/auth');
 
 dotenv.config();
 const app = express();
 
+// Allow frontend
 app.use(cors({ origin: 'http://localhost:3000' }));
+
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 
 mongoose.connect(process.env.MONGO_URI, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
-}).then(() => {
+})
+.then(() => {
 	console.log('MongoDB connected');
 	app.listen(process.env.PORT, () => {
-		console.log('Server running on port ${process.env.PORT}');
-});
-}).catch(err => console.error(err));
-
+		console.log(`Server running on port ${process.env.PORT}`);
+	});
+})
+.catch(err => console.error(err));

d5fb34325c7f7cae695643dfd05b70b740929bd4
Heaven <hjc4@njit.edu>
2025-11-16 17:09:35 -0500

    added backend files with MI

---
+++ tbr-backend/server.js
@@ -0,0 +1,25 @@
+
+
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require"('cors');
+const authRoutes = require('./routes/auth');
+
+dotenv.config();
+const app = express();
+
+app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(express.json());
+app.use('/api/auth', authRoutes);
+
+mongoose.connect(process.env.MONGO_URI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+}).then(() => {
+	console.log('MongoDB connected');
+	app.listen(process.env.PORT, () => {
+		console.log('Server running on port ${process.env.PORT}');
+});
+}).catch(err => console.error(err));
+

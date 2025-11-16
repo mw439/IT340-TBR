7c58f05918f83f80d640b5b902e5dc138aa906bb
MengChengWang <mw439@njit.edu>
2025-11-15 19:11:05 -0500

    Major Addition: Dark Mode Changes to app css, html, and ts files

--- tbr-frontend/src/app/app.ts
+++ tbr-frontend/src/app/app.ts
@@ -9,6 +9,18 @@
 //   styleUrls: []
 // })
 // export class App {}
+// import { Component } from '@angular/core';
+// import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+
+// @Component({
+//   selector: 'app-root',
+//   standalone: true,
+//   imports: [RouterOutlet, RouterLink, RouterLinkActive],
+//   templateUrl: './app.html',
+//   styleUrls: ['./app.css'],
+// })
+// export class App {}
+
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
@@ -19,4 +31,21 @@ import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
   templateUrl: './app.html',
   styleUrls: ['./app.css'],
 })
-export class App {}
+export class App {
+
+  // Run when the app loads
+  ngOnInit() {
+    const saved = localStorage.getItem('darkMode');
+    if (saved === 'true') {
+      document.body.classList.add('dark-mode');
+    }
+  }
+
+  // Toggle between light and dark mode
+  toggleDarkMode() {
+    document.body.classList.toggle('dark-mode');
+
+    const enabled = document.body.classList.contains('dark-mode');
+    localStorage.setItem('darkMode', enabled ? 'true' : 'false');
+  }
+}

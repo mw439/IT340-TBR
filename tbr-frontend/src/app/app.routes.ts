01ac37cf19ec034a72f93036f0d4c4793df01d21
MengCheng Wang <mw439@njit.edu>
2025-12-16 20:09:59 -0500

    page routing fixes frontend

--- tbr-frontend/src/app/app.routes.ts
+++ tbr-frontend/src/app/app.routes.ts
@@ -1,27 +1,81 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+
+  // ---------- PUBLIC PAGES ----------
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./pages/home/home').then(m => m.Home),
+  },
+  {
+    path: 'browse',
+    loadComponent: () =>
+      import('./pages/browse/browse').then(m => m.Browse),
+  },
+  {
+    path: 'search',
+    loadComponent: () =>
+      import('./pages/search/search').then(m => m.Search),
+  },
+  {
+    path: 'cart',
+    loadComponent: () =>
+      import('./pages/cart/cart').then(m => m.Cart),
+  },
+  {
+    path: 'checkout',
+    loadComponent: () =>
+      import('./pages/checkout/checkout').then(m => m.Checkout),
+  },
+  {
+    path: 'contact',
+    loadComponent: () =>
+      import('./pages/contact/contact').then(m => m.Contact),
+  },
+  {
+    path: 'about',
+    loadComponent: () =>
+      import('./pages/about/about').then(m => m.About),
+  },
+  {
+    path: 'book-details',
+    loadComponent: () =>
+      import('./pages/book-details/book-details').then(m => m.BookDetails),
+  },
+
+  // ---------- AUTH PAGES ----------
   {
     path: 'login',
     loadComponent: () =>
-      import('./pages/login/login').then((m) => m.Login),
+      import('./pages/login/login').then(m => m.Login),
   },
   {
     path: 'register',
     loadComponent: () =>
-      import('./pages/register/register').then((m) => m.Register),
+      import('./pages/register/register').then(m => m.Register),
   },
   {
-    path: 'home',
+    path: 'forgot-password',
     loadComponent: () =>
-      import('./pages/home/home').then((m) => m.Home),
+      import('./pages/forgot-password/forgot-password').then(m => m.ForgotPassword),
   },
+
+  // ---------- USER ----------
   {
     path: 'profile',
     loadComponent: () =>
-      import('./pages/profile/profile').then((m) => m.Profile),
+      import('./pages/profile/profile').then(m => m.Profile),
   },
 
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: '**', redirectTo: '/login' },
+  // ---------- FALLBACK ----------
+  {
+    path: 'not-found',
+    loadComponent: () =>
+      import('./pages/not-found/not-found').then(m => m.NotFound),
+  },
+
+  // ---------- DEFAULTS ----------
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/not-found' },
 ];
